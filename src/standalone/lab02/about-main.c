1a87ae6ae2e7d8eae0b97614703006874d02485b
Ahmed Sikh <ahmsi881@student.liu.se>
2022-03-29 16:42:27 +0200

    Tog bort lab2 ändringar från lab3 branchen

--- src/standalone/lab02/about-main.c
+++ src/standalone/lab02/about-main.c
@@ -2,20 +2,4 @@
 #include <string.h>
 
 int main(int argc, char* argv[])
-{
-    int total_chars = 0;
-    float total_args = 0;
-    
-    int i;
-    for (i = 0; i < argc; i++) {
-        int length = strlen(argv[i]);
-        total_chars += length;
-        total_args += 1;
-        printf("%-20s %4d\n", argv[i], length);
-    }
-
-    printf("%-20s %4d\n", "Total length", total_chars);
-    printf("%-20s %4.2f\n", "Average length", total_chars / total_args);
-
-    return total_chars;
-}
+{}
