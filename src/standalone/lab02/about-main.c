aede6f6882f5fe14fd8bc4e03f9f0cb0fbc43b45
Ahmed Sikh <ahmsi881@student.liu.se>
2022-03-29 16:26:00 +0200

    Klar med lab2

--- src/standalone/lab02/about-main.c
+++ src/standalone/lab02/about-main.c
@@ -3,4 +3,19 @@
 
 int main(int argc, char* argv[])
 {
+    int total_chars = 0;
+    float total_args = 0;
+    
+    int i;
+    for (i = 0; i < argc; i++) {
+        int length = strlen(argv[i]);
+        total_chars += length;
+        total_args += 1;
+        printf("%-20s %4d\n", argv[i], length);
+    }
+
+    printf("%-20s %4d\n", "Total length", total_chars);
+    printf("%-20s %4.2f\n", "Average length", total_chars / total_args);
+
+    return total_chars;
 }
